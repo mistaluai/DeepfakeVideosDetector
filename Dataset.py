04af4d2eea668dc4088f7ba5ace427458e2f9ee7
Luai Waleed <mistaluai@gmail.com>
2025-02-11 08:27:57 +0200

    initial dataset imp

---
+++ Dataset.py
@@ -0,0 +1,96 @@
+import itertools
+
+import torch
+import os
+import random
+
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision.datasets.folder import make_dataset
+from torchvision import transforms as t
+
+
+def get_samples(root, extensions=(".mp4", ".avi")):
+    samples = []
+
+    # Define class labels
+    class_to_idx = {
+        "DFD_original_sequences": 0,  # Real videos
+        "DFD_manipulated_sequences": 1  # Deepfake videos
+    }
+
+    for class_name, label in class_to_idx.items():
+        class_dir = os.path.join(root, class_name)
+        if class_name == 'DFD_manipulated_sequences':
+            class_dir = os.path.join(class_dir, class_name)
+        print(class_dir)
+        if not os.path.exists(class_dir):
+            continue
+
+        # Get all video files in the directory
+        for filename in os.listdir(class_dir):
+            if filename.endswith(extensions):
+                file_path = os.path.join(class_dir, filename)
+                samples.append((file_path, label))
+
+    return samples
+
+
+class RandomDataset(torch.utils.data.IterableDataset):
+    def __init__(self, root, epoch_size=None, frame_transform=None, video_transform=None, clip_len=16):
+        super(RandomDataset).__init__()
+
+        self.samples = get_samples(root)
+
+        # Allow for temporal jittering
+        if epoch_size is None:
+            epoch_size = len(self.samples)
+        self.epoch_size = epoch_size
+
+        self.clip_len = clip_len
+        self.frame_transform = frame_transform
+        self.video_transform = video_transform
+
+    def __iter__(self):
+        for i in range(self.epoch_size):
+            # Get random sample
+            path, target = random.choice(self.samples)
+            # Get video object
+            vid = torchvision.io.VideoReader(path, "video")
+            metadata = vid.get_metadata()
+            video_frames = []  # video frame buffer
+
+            # Seek and return frames
+            max_seek = metadata["video"]['duration'][0] - (self.clip_len / metadata["video"]['fps'][0])
+            start = random.uniform(0., max_seek)
+            for frame in itertools.islice(vid.seek(start), self.clip_len):
+                video_frames.append(self.frame_transform(frame['data']))
+                current_pts = frame['pts']
+            # Stack it into a tensor
+            video = torch.stack(video_frames, 0)
+            if self.video_transform:
+                video = self.video_transform(video)
+            output = {
+                'path': path,
+                'video': video,
+                'target': target
+            }
+            yield output
+
+
+
+## Example
+transforms = [t.Resize((224, 224))]
+frame_transform = t.Compose(transforms)
+
+dataset = RandomDataset("./dataset", epoch_size=None, frame_transform=frame_transform)
+
+loader = DataLoader(dataset, batch_size=12)
+data = {"video": [], 'start': [], 'end': [], 'tensorsize': []}
+for batch in loader:
+    for i in range(len(batch['path'])):
+        data['video'].append(batch['path'][i])
+        data['start'].append(batch['start'][i].item())
+        data['end'].append(batch['end'][i].item())
+        data['tensorsize'].append(batch['video'][i].size())
+print(data)
